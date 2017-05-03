4d8a10c224eec68491cfe5612c97d08170ad5c5f
HashiCorp Training <training@hashicorp.com>
2017-05-03 23:14:06 +0000

    all the files

---
+++ main.tf
@@ -0,0 +1,66 @@
+#
+# DO NOT DELETE THESE LINES!
+#
+# Your AMI ID is:
+#
+#     ami-2df66d3b
+#
+# Your subnet ID is:
+#
+#     subnet-fe40a9a4
+#
+# Your security group ID is:
+#
+#     sg-4cc10432
+#
+# Your Identity is:
+#
+#     testing-hawk
+#
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_instance" "web" {
+  count                  = "${var.num_webs}"
+  ami                    = "ami-2df66d3b"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-4cc10432"]
+  subnet_id              = "subnet-fe40a9a4"
+
+  tags {
+    Identity    = "testing-hawk"
+    CostCenter  = "hashicorp"
+    Environment = "hashitraining"
+    Name        = "web ${count.index + 1}/${var.num_webs}"
+  }
+}
+
+terraform {
+  backend "atlas" {
+    name = "rfackett/training"
+  }
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key" {}
+
+variable "aws_secret_key" {}
+
+variable "num_webs" {
+  default = 2
+}
+
+output "public_ip" {
+  value = ["${aws_instance.web.*.public_ip}"]
+}
+
+output "public_dns" {
+  value = ["${aws_instance.web.*.public_dns}"]
+}
